913f6fb2af3bb61fafb9d1f7a26a573f7451264f
Ahmed Hossam <ahmed.7oskaa@gmail.com>
2022-02-25 19:32:23 +0200

    Update files

--- Print_Template.cpp
+++ Print_Template.cpp
@@ -1,9 +1,8 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-#define cin(vec) for(auto& i : vec) cin >> i
 #define cin_2d(vec, n, m) for(int i = 0; i < n; i++) for(int j = 0; j < m && cin >> vec[i][j]; j++);
-#define cout(vec) for(auto& i : vec) cout << i << " "; cout << "\n";
 #define cout_2d(vec, n, m) for(int i = 0; i < n; i++, cout << "\n") for(int j = 0; j < m && cout << vec[i][j] << " "; j++);
 #define cout_map(mp) for(auto& [f, s] : mp) cout << f << "  " << s << "\n";
 #define Time cerr << "Time Taken: " << (float)clock() / CLOCKS_PER_SEC << " Secs" << "\n";
@@ -25,6 +24,16 @@ using namespace std;
 #define OO 2'000'000'000
 #define EPS 1e-4
 #define PI acos(-1)
+
+template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
+    for (auto &x: v) in >> x;
+    return in;
+}
+
+template < typename T = int > ostream& operator << (ostream &out, const vector < T > &v) { 
+    for (const T &x: v) out << x << ' '; 
+    return out;
+}
 
 void AhMeD_HoSSaM(){
     ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
@@ -295,7 +304,7 @@ struct Partial_2D {
 
 };
 
-// -------------------------- nCr power inverse -----------------------------
+// -------------------------- Power inverse -----------------------------
 
 struct Power_Inverse {
     
@@ -674,7 +683,7 @@ struct Fenwick_Tree {
 
 };
 
-// -------------------------- Fenwick Tree -----------------------------
+// -------------------------- Fenwick Tree 2D -----------------------------
 
 struct Fenwick_Tree_2D {
     
