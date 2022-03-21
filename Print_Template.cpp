095826d2c175572556a0f7008f17835f91c15897
Ahmed Hossam <ahmed.7oskaa@gmail.com>
2022-03-21 19:08:56 +0200

    Update files

--- Print_Template.cpp
+++ Print_Template.cpp
@@ -904,7 +904,7 @@ struct Floyd {
 
 };
 
-// -------------------------- Floyd -----------------------------
+// -------------------------- Kadane -----------------------------
 
 struct Kadane {
     
@@ -932,6 +932,37 @@ struct Kadane {
             Max_Curr = max(Max_Curr, 0ll);
         }
         return Max_so_far;
+    }
+
+};
+
+// -------------------------- Coordinate Compression -----------------------------
+
+struct Coordinate_Compression {
+
+    map < ll, ll > mapping;
+    vector < ll > orig, comp, nums;
+    int sz;
+
+    Coordinate_Compression(vector < ll >& Nums){
+        sz = 1;
+        nums = Nums;
+        sort(all(nums));
+        nums.resize(unique(all(nums)) - nums.begin());
+        for(auto& i : nums)
+            mapping[i] = sz++;
+        orig.resize(sz);
+        for(auto& [f, s] : mapping) orig[s] = f;
+        comp = nums;
+        for(auto& i : comp) i = mapping[i];
+    }
+
+    vector < ll > orignal(){
+        return orig;
+    }
+
+    vector < ll > compressed(){
+        return comp;
     }
 
 };
