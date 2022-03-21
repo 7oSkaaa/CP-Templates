cdb452f00fb9e0b5c2231fcad836baee14d779af
Ahmed Hossam <ahmed.7oskaa@gmail.com>
2022-03-21 19:07:52 +0200

    Update files

--- Print_Template.cpp
+++ Print_Template.cpp
@@ -246,30 +246,23 @@ struct Prefix_2D {
     int n, m;
     vector < vector < ll > > prefix;
     
-    Prefix_2D(int N, int M){
+    Prefix_2D(int N = 0, int M = 0){
         n = N, m = M;
         prefix.assign(n + 5, vector < ll > (m + 5));
-    }
-
-    void Get_Data(){
-        for(int i = 1; i <= n; i++)
-            for(int j = 1; j <= m; j++)
-                cin >> prefix[i][j];
     }
 
     // Get the sum of the number in the rectangle between x1, y1, x2, y2
 
     ll Get_Query(int x1, int y1, int x2, int y2){
+        if(x1 > x2) swap(x1, x2);
+        if(y1 > y2) swap(y1, y2);
         return prefix[x2][y2] - prefix[x1 - 1][y2] - prefix[x2][y1 - 1] + prefix[x1 - 1][y1 - 1];
     }
 
-    void Build_Prefix(){
+    void Build_Prefix(vector < vector < int > >& matrix){
         for(int i = 1; i <= n; i++)
             for(int j = 1; j <= m; j++)
-                prefix[i][j] += prefix[i][j - 1];
-        for(int j = 1; j <= m; j++)
-            for(int i = 1; i <= n; i++)
-                prefix[i][j] += prefix[i - 1][j];
+                prefix[i][j] = matrix[i - 1][j - 1] + prefix[i][j - 1] + prefix[i - 1][j] - prefix[i - 1][j - 1];
     }
 
     void Print_Prefix(){
