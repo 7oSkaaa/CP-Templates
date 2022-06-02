8b7388b18ff2344660741ac2ae475a671054cc09
7oSkaaa <ahmed.7oskaa@gmail.com>
2022-06-02 22:57:38 +0200

    add sparse_table.cpp

---
+++ Sparse_Table.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define cin_2d(vec, n, m) for(int i = 0; i < n; i++) for(int j = 0; j < m && cin >> vec[i][j]; j++);
+#define cout_2d(vec, n, m) for(int i = 0; i < n; i++, cout << "\n") for(int j = 0; j < m && cout << vec[i][j] << " "; j++);
+#define cout_map(mp) for(auto& [f, s] : mp) cout << f << "  " << s << "\n";
+#define Time cerr << "Time Taken: " << (float)clock() / CLOCKS_PER_SEC << " Secs" << "\n";
+#define fixed(n) fixed << setprecision(n)
+#define ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define fill(vec, value) memset(vec, value, sizeof(vec));
+#define Num_of_Digits(n) ((int)log10(n) + 1)
+#define mod_combine(a, b, m) (((a % m) * (b % m)) % m)
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define sz(x) int(x.size())
+#define debug(x) cout << #x << ": " << (x) << "\n";
+#define fi first
+#define se second
+#define Pair pair < int, int >
+#define ll long long
+#define ull unsigned long long
+#define Mod  1'000'000'007
+#define OO 2'000'000'000
+#define EPS 1e-9
+#define PI acos(-1)
+
+template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
+    for (auto &x: v) in >> x;
+    return in;
+}
+
+template < typename T = int > ostream& operator << (ostream &out, const vector < T > &v) { 
+    for (const T &x: v) out << x << ' '; 
+    return out;
+}
+
+void AhMeD_HoSSaM(){
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout); 
+    #endif
+}
+
+template < typename T = int > struct Sparse_Table {
+
+    vector < vector < T > > table;
+    vector < int > Bin_Log;
+    int n, LOG;
+
+    Sparse_Table(vector < T >& vec){
+        n = sz(vec);
+        LOG = __lg(n) + 1;
+        table.resize(n + 10, vector < T > (LOG));
+        Bin_Log.resize(n + 10);
+        for(int i = 2; i <= n; i++)
+            Bin_Log[i] = Bin_Log[i >> 1] + 1;
+        for(int i = 0; i < n; i++)
+            table[i][0] = vec[i];
+        Build_Table();
+    }
+
+    T operation(T a, T b){
+        return min(a, b);
+    }
+
+    void Build_Table(){
+        for(int log = 1; log < LOG; log++)
+            for(int i = 0; i + (1 << log) - 1 < n; i++)
+                table[i][log] = operation(table[i][log - 1], table[i + (1 << (log - 1))][log - 1]);
+    }
+
+    T query(int L, int R){
+        int log = Bin_Log[R - L + 1];
+        return operation(table[L][log], table[R - (1 << log) + 1][log]);
+    }
+
+};
+
+void Solve(){
+    
+}
+
+int main(){
+    AhMeD_HoSSaM();
+    int t = 1;
+    //cin >> t;
+    while(t--)
+        Solve();
+    return 0;
+}
