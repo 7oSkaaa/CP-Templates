5cd1f25252440efe6a767a59b233eff94e6712df
7oSkaaa <ahmed.7oskaa@gmail.com>
2022-06-02 23:10:53 +0200

    add template data type to Kadane.cpp

--- Kadane.cpp
+++ Kadane.cpp
@@ -42,30 +42,30 @@ void AhMeD_HoSSaM(){
     #endif
 }
 
-struct Kadane {
+template < typename T = int > struct Kadane {
     
     // Minimum Contigours Subarray Sum
 
     Kadane(){};
 
-    ll Min_Subarray_Sum(vector < ll >& nums){
-        ll Min_so_far = OO, Min_Curr = 0;
+    T Min_Subarray_Sum(vector < T >& nums){
+        T Min_so_far = OO, Min_Curr = 0, DEFAULT = 0;
         for(auto& i : nums){
             Min_Curr += i;
             Min_so_far = min(Min_so_far, Min_Curr);
-            Min_Curr = min(Min_Curr, 0ll);
+            Min_Curr = min(Min_Curr, DEFAULT);
         }
         return Min_so_far;
     }
 
     // Maximum Contigours Subarray Sum
 
-    ll Max_Subarray_Sum(vector < ll >& nums){
-        ll Max_so_far = -OO, Max_Curr = 0;
+    T Max_Subarray_Sum(vector < T >& nums){
+        T Max_so_far = -OO, Max_Curr = 0, DEFAULT = 0;
         for(auto& i : nums){
             Max_Curr += i;
             Max_so_far = max(Max_so_far, Max_Curr);
-            Max_Curr = max(Max_Curr, 0ll);
+            Max_Curr = max(Max_Curr, DEFAULT);
         }
         return Max_so_far;
     }
