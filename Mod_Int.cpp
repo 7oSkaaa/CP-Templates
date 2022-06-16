010b7803efd992b2fffc450f32099941054c2fd1
7oSkaaa <ahmed.7oskaa@gmail.com>
2022-06-16 23:55:17 +0200

    add new_template "Mod_Int.cpp"

---
+++ Mod_Int.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define cin_2d(vec, n, m) for(int i = 0; i < n; i++) for(int j = 0; j < m && cin >> vec[i][j]; j++);
+#define cout_2d(vec, n, m) for(int i = 0; i < n; i++, cout << "\n") for(int j = 0; j < m && cout << vec[i][j] << " "; j++);
+#define cout_map(mp) for(auto& [f, s] : mp) cout << f << "  " << s << "\n";
+#define Time cerr << "Time Taken: " << (float)clock() / CLOCKS_PER_SEC << " Secs" << "\n";
+#define fixed(n) fixed << setprecision(n)
+#define ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define fill(vec, value) memset(vec, value, sizeof(vec));
+#define Num_of_Digits(n) ((int)log10(n) + 1)
+#define mod_combine(a, b, m) (((a % m) * (b % m)) % m)
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define sz(x) int(x.size())
+#define debug(x) cout << #x << ": " << (x) << "\n";
+#define fi first
+#define se second
+#define ll long long
+#define ull unsigned long long
+#define Mod  1'000'000'007
+#define OO 2'000'000'000
+#define EPS 1e-9
+#define PI acos(-1)
+template < typename T = int > using Pair = pair < T, T >;
+
+template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
+    for (auto &x: v) in >> x;
+    return in;
+}
+
+template < typename T = int > ostream& operator << (ostream &out, const vector < T > &v) { 
+    for (const T &x: v) out << x << ' '; 
+    return out;
+}
+
+void AhMeD_HoSSaM(){
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout); 
+    #endif
+}
+
+template < ll MOD = 1'000'000'007 > struct ModInt {
+
+    ll val;
+
+    ModInt(ll V = 0, ll M = 1e9 + 7) : val(V) { val %= MOD; }
+
+    ModInt& operator += (const ModInt& rhs) {
+        if ((val += rhs.val) >= MOD) val -= MOD;
+        return *this;
+    }
+
+    ModInt& operator -= (const ModInt& rhs) { 
+        if ((val += MOD - rhs.val) >= MOD) val -= MOD; 
+        return *this; 
+    }
+
+    ModInt& operator *= (const ModInt& rhs) { val = (val * rhs.val) % MOD; return *this; }
+
+    ModInt& operator /= (const ModInt& rhs) { return *this *= rhs.inverse(); }
+
+    ModInt operator + (const ModInt& rhs) const { ModInt res(*this); return res += rhs; }
+
+    ModInt operator - (const ModInt& rhs) const { ModInt res(*this); return res -= rhs; }
+
+    ModInt operator * (const ModInt& rhs) const { ModInt res(*this); return res *= rhs; }
+
+    ModInt operator / (const ModInt& rhs) const { ModInt res(*this); return res /= rhs; }
+
+    bool operator == (const ModInt& rhs) const { return val == rhs.val; }
+
+    bool operator != (const ModInt& rhs) const { return val != rhs.val; }
+
+    ModInt inverse() const { return pow(MOD - 2); }
+
+    ModInt pow(ll n) const {
+        ModInt a = *this, res = 1;
+        while (n > 0) {
+            if (n & 1) res *= a;
+            a *= a, n >>= 1;
+        }
+        return res;
+    }
+
+    friend std::istream& operator>>(std::istream& is, ModInt& x) noexcept { return is >> x.val; }
+    friend std::ostream& operator<<(std::ostream& os, const ModInt& x) noexcept { return os << x.val; }
+
+};
+
+using Mint = ModInt < >;
+
+void Solve(){
+    
+}
+
+int main(){
+    AhMeD_HoSSaM();
+    int t = 1;
+    //cin >> t;
+    while(t--)
+        Solve();
+    return 0;
+}
