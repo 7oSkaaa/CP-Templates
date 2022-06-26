2645bfa44a0644fe6bf27c8cac22415dff608cdb
7oSkaaa <ahmed.7oskaa@gmail.com>
2022-06-26 21:17:26 +0200

    add some function in Mod_int.cpp

--- Mod_Int.cpp
+++ Mod_Int.cpp
@@ -52,39 +52,76 @@ template < int MOD = 1000000007 > struct ModInt {
         if ((val += rhs.val) >= MOD) val -= MOD;
         return *this;
     }
+    ModInt& operator += (const int rhs) {
+        if ((val += rhs) >= MOD) val -= MOD;
+        return *this;
+    }
 
     ModInt& operator -= (const ModInt& rhs) { 
         if ((val += MOD - rhs.val) >= MOD) val -= MOD; 
         return *this; 
     }
+    ModInt& operator -= (const int rhs) { 
+        if ((val += MOD - rhs) >= MOD) val -= MOD; 
+        return *this; 
+    }
 
     ModInt& operator *= (const ModInt& rhs) { val = (1ll * val * rhs.val) % MOD; return *this; }
+    ModInt& operator *= (const int rhs) { val = (1ll * val * rhs) % MOD; return *this; }
 
     ModInt& operator /= (const ModInt& rhs) { return *this *= rhs.inverse(); }
+    ModInt& operator /= (const int rhs) { return *this *= ModInt(rhs).inverse(); }
 
-    ModInt& operator %= (const ModInt& rhs) { return *this.val %= rhs.val; }
+    ModInt& operator %= (const ModInt& rhs) { val %= rhs.val; return *this; }
+    ModInt& operator %= (const int rhs) { val %= rhs; return *this; }
 
+    ModInt& operator ++() { return *this += 1; }
+    ModInt& operator --() { return *this -= 1; }
+ 
+    ModInt operator ++(int unused) { ModInt res(*this); ++*this; return res; }
+    ModInt operator --(int unused) { ModInt res(*this); --*this; return res; }
+    
     ModInt operator + (const ModInt& rhs) const { ModInt res(*this); return res += rhs; }
+    ModInt operator + (const int rhs) const { ModInt res(*this); return res += rhs; }
 
     ModInt operator % (const ModInt& rhs) const { ModInt res(*this); return res %= rhs; }
+    ModInt operator % (const int rhs) const { ModInt res(*this); return res %= rhs; }
 
     ModInt operator - (const ModInt& rhs) const { ModInt res(*this); return res -= rhs; }
+    ModInt operator - (const int rhs) const { ModInt res(*this); return res -= rhs; }
 
     ModInt operator * (const ModInt& rhs) const { ModInt res(*this); return res *= rhs; }
+    ModInt operator * (const int rhs) const { ModInt res(*this); return res *= rhs; }
 
     ModInt operator / (const ModInt& rhs) const { ModInt res(*this); return res /= rhs; }
+    ModInt operator / (const int rhs) const { ModInt res(*this); return res /= rhs; }
+
+    ModInt& operator = (const ModInt& rhs) { val = rhs.val; return *this; }
+    ModInt& operator = (const int rhs) { val = rhs; return *this; }
 
     bool operator == (const ModInt& rhs) const { return val == rhs.val; }
+    bool operator == (const int rhs) const { return val == rhs; }
 
     bool operator != (const ModInt& rhs) const { return val != rhs.val; }
+    bool operator != (const int rhs) const { return val != rhs; }
 
     bool operator < (const ModInt& rhs) const { return val < rhs.val; }
+    bool operator < (const int rhs) const { return val < rhs; }
+
+    bool operator <= (const ModInt& rhs) const { return val <= rhs.val; }
+    bool operator <= (const int rhs) const { return val <= rhs; }
 
     bool operator > (const ModInt& rhs) const { return val > rhs.val; }
+    bool operator > (const int rhs) const { return val > rhs; }
 
-    ModInt inverse() const { return pow(MOD - 2); }
+    bool operator >= (const ModInt& rhs) const { return val >= rhs.val; }
+    bool operator >= (const int rhs) const { return val >= rhs; }
 
-    ModInt pow(ll n) const {
+    int operator () () const { return val; }
+
+    ModInt inverse() const { return power(MOD - 2); }
+
+    ModInt power(ll n) const {
         ModInt a = *this, res = 1;
         while (n > 0) {
             if (n & 1) res *= a;
@@ -93,11 +130,24 @@ template < int MOD = 1000000007 > struct ModInt {
         return res;
     }
 
+    ModInt power(ModInt n) const {
+        ModInt a = *this, res = 1;
+        int e = n();
+        while (e > 0) {
+            if (e & 1) res *= a;
+            a *= a, e >>= 1;
+        }
+        return res;
+    }
+
+    friend ModInt operator ^ (ModInt rhs, ll n) { return rhs.power(n); }
+    friend ModInt operator ^ (ModInt rhs, ModInt n) { return rhs.power(n); }
+
     friend std::istream& operator>>(std::istream& is, ModInt& x) noexcept { return is >> x.val; }
     friend std::ostream& operator<<(std::ostream& os, const ModInt& x) noexcept { return os << x.val; }
 
 };
-using Mint = ModInt < 1000000007 >;
+using Mint = ModInt < 998244353 >;
 
 void Solve(){
     
