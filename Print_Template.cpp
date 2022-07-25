783e802923a5ecb40acc33d3230f046b0799b955
7oSkaaa <ahmed.7oskaa@gmail.com>
2022-07-25 23:02:48 +0200

    add some optimization to print_Template.cpp

--- Print_Template.cpp
+++ Print_Template.cpp
@@ -1972,10 +1972,13 @@ using ordered_multiset = ordered_multimap<K, null_type, Comp>;
 
 // -------------------------- Optimizations -----------------------------
 
-#pragma GCC optimize("Ofast")
-#pragma GCC target("avx,avx2,fma")
-#pragma GCC optimization ("unroll-loops")
-#pragma GCC target("avx2,bmi,bmi2,popcnt,lzcnt")
+#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math,O3") // GCC
+#undef _GLIBCXX_DEBUG       // disable run-time bound checking, etc
+#pragma GCC optimize("Ofast,inline")        // Ofast = O3,fast-math,allow-store-data-races,no-protect-parens
+#pragma GCC target("bmi,bmi2,lzcnt,popcnt")     // bit manipulation
+#pragma GCC target("movbe")     // byte swap
+#pragma GCC target("aes,pclmul,rdrnd")      // encryption
+#pragma GCC target("avx,avx2,f16c,fma,sse3,ssse3,sse4.1,sse4.2")        // SIMD
 
 // -------------------------- Directions -----------------------------
  
