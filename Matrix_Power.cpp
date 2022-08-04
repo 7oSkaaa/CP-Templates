1a79577a774cc56bbbed6dd4122d656e2eaaba3d
7oSkaaa <ahmed.7oskaa@gmail.com>
2022-08-04 09:26:33 +0200

    inital matrix_power.cpp

---
+++ Matrix_Power.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define cin_2d(vec, n, m) for(int i = 0; i < n; i++) for(int j = 0; j < m && cin >> vec[i][j]; j++);
+#define cout_2d(vec, n, m) for(int i = 0; i < n; i++, cout << "\n") for(int j = 0; j < m && cout << vec[i][j] << " "; j++);
+#define fixed(n) fixed << setprecision(n)
+#define ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define fill(vec, value) memset(vec, value, sizeof(vec));
+#define mul_mod(a, b, m) (((a % m) * (b % m)) % m)
+#define add_mod(a, b, m) (((a % m) + (b % m)) % m)
+#define all(vec) vec.begin(), vec.end()
+#define rall(vec) vec.rbegin(), vec.rend()
+#define sz(x) int(x.size())
+#define debug(x) cout << #x << ": " << (x) << "\n";
+#define fi first
+#define se second
+#define ll long long
+#define ull unsigned long long
+#define Mod  1'000'000'007
+#define OO 2'000'000'000
+#define EPS 1e-9
+#define PI acos(-1)
+template < typename T = int > using Pair = pair < T, T >;
+vector < string > RET = {"NO", "YES"};
+
+template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
+    for (auto &x : v) in >> x;
+    return in;
+}
+
+template < typename T = int > ostream& operator << (ostream &out, const vector < T > &v) { 
+    for (const T &x : v) out << x << ' '; 
+    return out;
+}
+
+void multiply(vector < vector < ll > >& F, vector < vector < ll > >& M){
+    ll x = F[0][0] * M[0][0] + F[0][1] * M[1][0];
+    ll y = F[0][0] * M[0][1] + F[0][1] * M[1][1];
+    ll z = F[1][0] * M[0][0] + F[1][1] * M[1][0];
+    ll w = F[1][0] * M[0][1] + F[1][1] * M[1][1];
+    F = {{x, y}, {z, w}};
+}
+
+void mat_power(vector < vector < ll > >& F, int n){
+    if(n == 0 || n == 1) return;
+    vector < vector < ll > > M = {{1, 1}, {1, 0}};
+    mat_power(F, n / 2);
+    multiply(F, F);
+    if (n % 2 != 0)
+        multiply(F, M);
+}
+
+ll fib(int n){
+    if (n == 0) return 0;
+    vector < vector < ll > > F = {{1, 1}, {1, 0}};
+    mat_power(F, n - 1);
+    return F[0][0];
+}
+
+void Solve(){
+    int n;
+    cin >> n;
+    cout << fib(n) << "\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int t = 1;
+    //cin >> t;
+    while(t--)
+        Solve();
+    return 0;
+}
