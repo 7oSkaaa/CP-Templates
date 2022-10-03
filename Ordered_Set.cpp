5526f19ab598e146de3ab07f5e2f6ca3f63cf694
Ahmed Hossam <ahmed_7oskaa@pop-os.localdomain>
2022-10-03 23:24:18 +0200

    edit:

--- Ordered_Set.cpp
+++ Ordered_Set.cpp
@@ -20,8 +20,9 @@ using ordered_multiset = ordered_multimap<K, null_type, Comp>;
 #define fixed(n) fixed << setprecision(n)
 #define ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
 #define fill(vec, value) memset(vec, value, sizeof(vec));
-#define mul_mod(a, b, m) (((a % m) * (b % m)) % m)
 #define add_mod(a, b, m) (((a % m) + (b % m)) % m)
+#define sub_mod(a, b, m) (((a % m) - (b % m) + m) % m)
+#define mul_mod(a, b, m) (((a % m) * (b % m)) % m)
 #define all(vec) vec.begin(), vec.end()
 #define rall(vec) vec.rbegin(), vec.rend()
 #define sz(x) int(x.size())
@@ -47,9 +48,102 @@ template < typename T = int > ostream& operator << (ostream &out, const vector <
     return out;
 }
 
+template < typename T = int , typename Comp = std::less_equal<T> > struct Ordered_Multiset {
+    ordered_multiset < T, Comp > mst;
+    
+    // Constructor 
+    Ordered_Multiset() {
+        mst.clear();
+    }
+    
+    // Constructor with vector
+    Ordered_Multiset(vector < T > &vec) {
+        mst.clear();
+        for (auto &x : vec) 
+            mst.insert(x);
+    }
 
-//order_of_key(val)  count elements smaller than val
-// *s.find_by_order(idx)  element with index idx
+    // Insert element
+    void insert(T val) {
+        mst.insert(val);
+    }
+
+    // check if element exists or not
+    bool is_exist(T val){ 
+        if((mst.upper_bound(val)) == mst.end())
+            return false;
+        return ((*mst.upper_bound(val)) == val);
+    }
+
+    // erase element if exists
+    void erase(T val){
+        if(is_exist(val))
+            mst.erase(mst.upper_bound(val));
+    }
+    
+    // return element with given index
+    T at(int idx){
+        return (*mst.find_by_order(idx));
+    }
+
+    // return element with given index like mst[idx]
+    T operator [] (int idx){
+        return at(idx);
+    }
+
+    // return first index of element
+    int first_idx(T val){ 
+        if(!is_exist(val))
+            return -1;
+        return (mst.order_of_key(val));
+    }
+
+    // return last index of element
+    int last_idx(T val){
+        if(!is_exist(val))
+            return -1;
+        if(at(sz(mst) - 1) == val)
+            return sz(mst) -1;
+        return first_idx(*mst.lower_bound(val)) - 1;
+    }
+
+    // return number of occurences of element
+    T count(T val){ 
+        if(!is_exist(val))
+            return 0;
+        return last_idx(val) - fisrt_idx(val) + 1;
+    }
+
+    // clear the ordered multiset
+    void clear(){
+        mst.clear();
+    }
+
+    // return size of ordered multiset
+    int size(){
+        return sz(mst);
+    }
+
+    // return number of elements < Comp > of val
+    int order_of_key(T val){
+        return mst.order_of_key(val);
+    }
+
+    // return iterator to element with given index
+    ordered_multiset < T, Comp >::iterator find_by_order(int idx){
+        return mst.find_by_order(idx);
+    }
+
+    // print the ordered multiset
+    friend ostream& operator << (ostream &out, const Ordered_Multiset < T, Comp > &mst) { 
+        for (const T &x : mst.mst) out << x << ' '; 
+        return out;
+    }
+
+    // Ordered_Multiset < int, less_equal < int > > mst;
+    // Ordered_Multiset < int, greater_equal < int > > mst;
+
+};
 
 void Solve(){
     
@@ -58,7 +152,7 @@ void Solve(){
 int main(){
     ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
     int t = 1;
-    //cin >> t;
+    // cin >> t;
     while(t--)
         Solve();
     return 0;
