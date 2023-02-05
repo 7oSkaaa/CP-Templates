c615c798502aee4ed4cbae3a4d4e480ffe149c3e
7oSkaaa <ahmed.7oskaa@gmail.com>
2023-02-05 18:59:08 +0200

    refactor: update Coordinate Compression

--- Coordinate_Compression.cpp
+++ Coordinate_Compression.cpp
@@ -37,6 +37,7 @@ template < typename T = int > ostream& operator << (ostream &out, const vector <
 template < typename T = int > struct Coordinate_Compression {
 
     vector < T > compressed;
+    bool is_build = true;
 
     Coordinate_Compression(){}
 
@@ -47,18 +48,22 @@ template < typename T = int > struct Coordinate_Compression {
 
     void add(T x) {
         compressed.push_back(x);
+        is_build = false;
     }
 
     void build() {
         sort(all(compressed));
         compressed.resize(unique(all(compressed)) - compressed.begin());
+        is_build = true;
     }
 
     T get(T x) {
+        if(!is_build) build();
         return upper_bound(all(compressed), x) - compressed.begin();
     }
 
     vector < T > get_compressed(vector < T > &vec) {
+        if(!is_build) build();
         vector < T > ret;
         for (auto &x : vec) 
             ret.push_back(get(x));
@@ -66,6 +71,7 @@ template < typename T = int > struct Coordinate_Compression {
     }
 
     vector < T > get_mapping(vector < T > &vec) {
+        if(!is_build) build();
         vector < T > ret(sz(compressed) + 5);
         for (auto &x : vec)
             ret[get(x)] = x;
