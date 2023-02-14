4b8e78f2327c39f15ed117a7c0ba6f3bb44cebdd
7oSkaaa <ahmed.7oskaa@gmail.com>
2023-02-14 23:20:34 +0200

    refactor: update Convex Hull Trick

--- Convex_Hull_Trick.cpp
+++ Convex_Hull_Trick.cpp
@@ -35,53 +35,53 @@ template < typename T = int > ostream& operator << (ostream &out, const vector <
     return out;
 }
 
-struct Convex_Hull_Trick {
+struct Line {
 
-    struct Line {
+    mutable ll m, c, p;
 
-        ll slope, yIntercept;
+    bool operator < (const Line &o) const { 
+        return m < o.m; 
+    }
 
-        Line(ll S, ll Y) : slope(S), yIntercept(Y) {}
+    bool operator < (ll x) const { 
+        return p < x; 
+    }
 
-        ll val(ll x){
-            return slope * x + yIntercept;
-        }
+    ll val(ll x) const { 
+        return m * x + c; 
+    }
+};
 
-        ll inersect(Line other){
-            return (other.yIntercept - yIntercept + (slope - other.slope - 1)) / (slope - other.slope);
-        }
-
-    };
+template < typename T = int, T Mode = -1 > class CHT : multiset < Line, less < > > {
+private:
     
-    deque < pair < Line, ll > > lines;
+    static constexpr T LLimit = numeric_limits < T > :: min(), RLimit = numeric_limits < T > :: max();
 
-    void insert(ll slope, ll yIntercept){
-        
-        Line newLine = Line(slope, yIntercept);
-        
-        while (sz(lines) > 1 && lines.back().second >= lines.back().first.inersect(newLine)) 
-            lines.pop_back();
-        
-        if (lines.empty()) 
-            lines.push_back({newLine, 0});
-        else 
-            lines.push_back({newLine, lines.back().first.inersect(newLine)});
+    T div(T a, T b) { // floored division
+        return a / b - ((a ^ b) < 0 && a % b);
     }
 
-    ll query(ll x){
-            
-        while (sz(lines) > 1 && lines[1].second <= x) 
-            lines.pop_front();
-        
-        return lines[0].first.val(x);
+    bool is_intersect(iterator x, iterator y) {
+        if (y == end()) return x -> p = RLimit, 0;
+        if (x -> m == y -> m) x -> p = (x -> c > y -> c ? RLimit : LLimit);
+        else x -> p = div(y -> c - x -> c, x -> m - y -> m);
+        return x -> p >= y -> p;
     }
 
-    ll query_bs(ll x){
-        auto query = *lower_bound(all(lines), make_pair(Line(0, 0), x), 
-            [&](const pair < Line, ll >& a, const pair < Line, ll >& b){
-                return a.second > b.second;
-        });
-        return query.first.val(x);
+public:
+    
+    void add(T m, T c) {
+        m *= Mode, c *= Mode;
+        auto z = insert({m, c, 0}), y = z++, x = y;
+        while (is_intersect(y, z)) z = erase(z);
+        if (x != begin() && is_intersect(--x, y)) is_intersect(x, y = erase(y));
+        while ((y = x) != begin() && (--x) -> p >= y -> p) is_intersect(x, erase(y));
+    }
+
+    T query(T x) {
+        assert(!empty());
+        auto l = *lower_bound(x);
+        return Mode * l.val(x);
     }
 
 };
