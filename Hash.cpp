bc5d2bf18a6932a6a6bd2032ae0793c3f49e58bd
7oSkaaa <ahmed.7oskaa@gmail.com>
2023-03-30 22:33:24 +0200

    Update Hash.cpp file

--- Hash.cpp
+++ Hash.cpp
@@ -32,50 +32,82 @@ template < typename T = int > ostream& operator << (ostream &out, const vector <
     return out;
 }
 
-template < int Mode = 0, int p = 31, int mod = 1000000009 > struct Hash {
-
-    int n, init;
-    vector < ll > p_pow, hash;                    
-
-    void init_(){
-        if(Mode == 0)
-            init = 'a';
-        else if(Mode == 1)
-            init = 'A';
-        else if(Mode == 2)
-            init = '0';
-        else 
-            init = 0;
-    }
-
-    Hash(string const& s){
-        init_();
+template < typename T = long long , int Base = 0 > struct Hash {
+    
+    int n;
+    vector < T > pow1, pow2, h1, h2;
+    const T p1 = 313, p2 = 1013;
+    const T m1 = 1e9 + 7, m2 = 1e9 + 9;
+ 
+    Hash(const string& s){
         n = s.size();
-        p_pow = hash = vector < ll > (n + 1);
-        p_pow[0] = 1;
-        for (int i = 1; i <= n; i++) 
-            p_pow[i] = (p_pow[i - 1] * p) % mod;
-        for (int i = n - 1; i >= 0; i--)
-            hash[i] = ((s[i] - init + 1) + hash[i + 1] * p_pow[1]) % mod;
+        h1 = h2 = pow1 = pow2 = vector < T > (n + 5);
+
+        // calulcate the powers
+        pow1[0] = pow2[0] = 1;
+        for(int i = 1; i <= n; i++) {
+            pow1[i] = (pow1[i - 1] * p1) % m1;
+            pow2[i] = (pow2[i - 1] * p2) % m2;
+        }
+
+        // calculate the hashes
+        h1[0] = h2[0] = 1;
+        for(int i = 1; i <= n; i++) {
+            h1[i] = (h1[i - 1] * p1 + s[i - !Base]) % m1;
+            h2[i] = (h2[i - 1] * p2 + s[i - !Base]) % m2;
+        }
     }
 
-    Hash(vector < ll > const& v){
-        init_();
-        n = v.size();
-        p_pow = hash = vector < ll > (n + 1);
-        p_pow[0] = 1;
-        for (int i = 1; i <= n; i++) 
-            p_pow[i] = (p_pow[i - 1] * p) % mod;
-        for (int i = n - 1; i >= 0; i--)
-            hash[i] = ((v[i] - init + 1) + hash[i + 1] * p_pow[1]) % mod;
+    Hash(const vector < T >& vec){
+        n = vec.size();
+        h1 = h2 = pow1 = pow2 = vector < T > (n + 5);
+        
+        // calulcate the powers
+        pow1[0] = pow2[0] = 1;
+        for(int i = 1; i <= n; i++) {
+            pow1[i] = (pow1[i - 1] * p1) % m1;
+            pow2[i] = (pow2[i - 1] * p2) % m2;
+        }
+
+        // calculate the hashes
+        h1[0] = h2[0] = 1;
+        for(int i = 1; i <= n; i++) {
+            h1[i] = (h1[i - 1] * p1 + vec[i - !Base]) % m1;
+            h2[i] = (h2[i - 1] * p2 + vec[i - !Base]) % m2;
+        }
     }
 
-    ll sub(int l, int r){
-        return ((hash[l - 1] - hash[r] * p_pow[r - l + 1]) % mod + mod) % mod;
+    // get the hash of substring [i, j]
+    pair < T, T > sub(int l, int r) {
+        // first hash
+        T F = h1[r];
+        F -= h1[l - 1] * pow1[r - l + 1];
+        F = ((F % m1) + m1) % m1;
+        
+        // second hash
+        T S = h2[r];
+        S -= h2[l - 1] * pow2[r - l + 1];
+        S = ((S % m2) + m2) % m2;
+        
+        return {F, S};
     }
-
-    ll at(int idx){
+ 
+    // merge two substrings
+    pair < T, T > merge_hash(int l1, int r1, int l2, int r2) {
+        auto a = sub(l1, r1), b = sub(l2, r2);
+        ll F = ((a.first * pow1[r2 - l2 + 1]) + b.first) % m1;
+        ll S = ((a.second * pow2[r2 - l2 + 1]) + b.second) % m2;
+        return {F, S};
+    }
+ 
+    // get the hash of the idx'th character
+    pair < T, T > at(int idx){
         return sub(idx, idx);
+    }
+
+    // check if two parts are equals
+    bool equal(int l1, int r1, int l2, int r2) {
+        return sub(l1, r1) == sub(l2, r2);
     }
 
 };
