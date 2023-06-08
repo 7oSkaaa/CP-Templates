b1d0e6346f82d488738ffbc0219d230f26a1c833
7oSkaaa <ahmed.7oskaa@gmail.com>
2023-06-08 18:18:15 +0300

    feat: update DSU

--- DSU.cpp
+++ DSU.cpp
@@ -36,12 +36,14 @@ template < typename T = int > ostream& operator << (ostream &out, const vector <
 
 template < typename T = int, int Base = 1 > struct DSU {
     
-    vector < T > parent, Gsize;
+    vector < T > parent, Gsize, nxt, tail, pos, roots;
 
     DSU(int MaxNodes){
-        parent = Gsize = vector < T > (MaxNodes + 5);
-        for(int i = Base; i <= MaxNodes; i++)
-          parent[i] = i, Gsize[i] = 1;
+        parent = Gsize = roots = tail = pos = nxt = vector < T > (MaxNodes + Base);
+        for(int i = Base; i < MaxNodes + Base; i++){
+            parent[i] = roots[i] = pos[i] = tail[i] = i;
+            nxt[i] = -1, Gsize[i] = 1;
+        }
     }
     
     T find_leader(int node){
@@ -55,8 +57,24 @@ template < typename T = int, int Base = 1 > struct DSU {
     void union_sets(int u, int v){
         int leader_u = find_leader(u), leader_v = find_leader(v);
         if(leader_u == leader_v) return;
-        if(Gsize[leader_u] < Gsize[leader_v]) swap(leader_u, leader_v);
-        Gsize[leader_u] += Gsize[leader_v], parent[leader_v] = leader_u;
+        // make leader_u is the leader with the larger component
+        if(Gsize[leader_u] < Gsize[leader_v]) 
+            swap(leader_u, leader_v);
+        int p = pos[leader_v];
+        Gsize[leader_u] += Gsize[leader_v];
+        parent[leader_v] = leader_u;
+        roots[p] = roots.back();
+        pos[roots[p]] = p;
+        roots.pop_back();
+        nxt[tail[leader_u]] = leader_v;
+        tail[leader_u] = tail[leader_v];
+    }
+
+    void print(){
+        for(int root = Base; root < sz(roots); root++){
+            for(int u = roots[root]; ~u ; u = nxt[u])
+                cout << u << " \n"[!~nxt[u]];
+        }
     }
 
     int get_size(int u){
