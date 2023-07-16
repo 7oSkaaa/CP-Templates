aafe347540db336664fac555a08355c85d8cb9ee
7oSkaaa <ahmed.7oskaa@gmail.com>
2023-07-16 14:05:23 +0300

    refactor: update SQRT_Decom

--- SQRT_Decomp.cpp
+++ SQRT_Decomp.cpp
@@ -34,11 +34,12 @@ template < typename T = int > ostream& operator << (ostream &out, const vector <
 
 template < typename T = int, int Base = 0 > struct Sqrt_Decomp {
 
-    int n;
-    vector < T > a, b;
-    int len;
+    int n, len;
+    vector < T > a; 
+    vector < vector < T > > b;
     T U_Default, Q_Default;
 
+    // calculate the length of each block
     T calc_sq(int N){
         int sq = sqrt(N);
         return sq * sq == N ? sq : sq + 1;
@@ -47,53 +48,53 @@ template < typename T = int, int Base = 0 > struct Sqrt_Decomp {
     Sqrt_Decomp(int N = 0){
         n = N, len = calc_sq(n), U_Default = 0, Q_Default = 0;
         a = vector < T > (n + 5, U_Default);
-        b = vector < T > (len + 5, Q_Default);
+        b = vector < vector < T > > (len + 5);
     }
 
     Sqrt_Decomp(int N, const vector < T >& vec){
-        n = N, len = calc_sq(n), U_Default = 0, Q_Default = INF;
+        n = N, len = calc_sq(n), U_Default = 0, Q_Default = 0;
         a = vec;
-        b = vector < T > (len + 5, Q_Default);
+        b = vector < vector < T > > (len + 5);
+        build();
     }
 
-    T update_operation(const T& L, const T& R){
-        return R;
-    }
-
-    T query_operation(const T& L, const T& R){
-        return min(L, R);
-    }
-
+    // build each block
     void build(){
         for(int i = 1; i <= n; i++)
-            b[i / len] = query_operation(b[i / len], a[i - !Base]);
+            b[i / len].push_back(a[i - !Base]);
+        for(int i = 0; i <= len; i++)
+            sort(all(b[i]));
     }
 
-    void update_1(int idx, T val){
-        b[idx / len] = query_operation(b[idx / len], val - a[idx - !Base]);
+    // just update this index in the block O(1)
+    void update(int idx, T val){
+        int idx2 = lower_bound(all(b[idx / len]), a[idx - !Base]) - b[idx / len].begin();
+        b[idx / len][idx2] = a[idx - !Base] = val;
+        sort(all(b[idx / len]));
+    }
+
+    // re-calculate the block again O(sqrt(n))
+    void update_range(int idx, T val){
         a[idx - !Base] = val;
+        b[idx / len].clear();
+        for(int i = idx / len * len; i < min(n, (idx / len + 1) * len); i++)
+            b[idx / len].push_back(a[i - !Base]);
+        sort(all(b[idx / len]));
     }
 
-    void update_sq(int idx, T val){
-        int b_id = idx / len;
-        int b_l = b_id * len, b_r = b_l + len - 1;
-        a[idx - !Base] = update_operation(a[idx - !Base], val);
-        b[b_id] = Q_Default;
-        for(int i = b_l; i <= min(b_r, n); i++)
-            b[b_id] = query_operation(b[b_id], a[i - !Base]);
-    }
-
-    T query(int l, int r){
+    // query on the range from L to R
+    T query(int l, int r, T x){
         T res = Q_Default;
         while(l < r && l % len != 0)
-            res = query_operation(res, a[l++ - !Base]);
-        while(l + len <= r)
-            res = query_operation(res, b[l / len]), l += len;
+            res += a[l++ - !Base] >= x;
+        while(l + len <= r){
+            res += sz(b[l / len]) - (lower_bound(all(b[l / len]), x) - b[l / len].begin());
+            l += len;
+        }
         while(l <= r)
-            res = query_operation(res, a[l++ - !Base]);
+            res += a[l++ - !Base] >= x;
         return res;
     }
-
 };
 
 void Solve(){
