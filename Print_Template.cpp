0fe657841e3516327e383f5f586cfeeec246da3e
7oSkaaa <ahmed.7oskaa@gmail.com>
2023-08-13 00:07:22 +0300

    update templates

--- Print_Template.cpp
+++ Print_Template.cpp
@@ -1176,6 +1176,32 @@ struct Math {
         return powers;
     }
 
+    // extended euclidean algorithm and diofantian equation
+    int extended_gcd(int a, int b, int& x, int& y) {
+        if (b == 0) {
+            x = 1;
+            y = 0;
+            return a;
+        }
+        int x1, y1;
+        int d = extended_gcd(b, a % b, x1, y1);
+        x = y1;
+        y = x1 - y1 * (a / b);
+        return d;
+    }
+    
+    bool find_any_solution(int a, int b, int c, int &x0, int &y0, int &g) {
+        g = extended_gcd(abs(a), abs(b), x0, y0);
+        if (c % g) {
+            return false;
+        }
+        x0 *= c / g;
+        y0 *= c / g;
+        if (a < 0) x0 = -x0;
+        if (b < 0) y0 = -y0;
+        return true;
+    }
+
     // Convert Decimal to any base
 
     string decimal_to_any_base(ll decimal, ll base){
