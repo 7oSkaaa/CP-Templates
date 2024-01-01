855065640b9e0d087f3856f4029e7f6e599c82ae
7oSkaaa <ahmed.7oskaa@gmail.com>
2024-01-01 19:05:06 +0200

    add KMP

--- KMP.cpp
+++ KMP.cpp
@@ -2,77 +2,110 @@
 
 using namespace std;
 
-#define cin_2d(vec, n, m) for(int i = 0; i < n; i++) for(int j = 0; j < m && cin >> vec[i][j]; j++);
-#define cout_2d(vec, n, m) for(int i = 0; i < n; i++, cout << "\n") for(int j = 0; j < m && cout << vec[i][j] << " "; j++);
-#define cout_map(mp) for(auto& [f, s] : mp) cout << f << "  " << s << "\n";
-#define Time cerr << "Time Taken: " << (float)clock() / CLOCKS_PER_SEC << " Secs" << "\n";
 #define fixed(n) fixed << setprecision(n)
-#define ceil(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
-#define fill(vec, value) memset(vec, value, sizeof(vec));
-#define Num_of_Digits(n) ((int)log10(n) + 1)
-#define mod_combine(a, b, m) (((a % m) * (b % m)) % m)
+#define ceil(n, m) (((n) + (m) - 1) / (m))
+#define add_mod(a, b, m) (((a % m) + (b % m)) % m)
+#define sub_mod(a, b, m) (((a % m) - (b % m) + m) % m)
+#define mul_mod(a, b, m) (((a % m) * (b % m)) % m)
 #define all(vec) vec.begin(), vec.end()
 #define rall(vec) vec.rbegin(), vec.rend()
 #define sz(x) int(x.size())
 #define debug(x) cout << #x << ": " << (x) << "\n";
 #define fi first
 #define se second
-#define Pair pair < int, int >
 #define ll long long
 #define ull unsigned long long
-#define Mod  1'000'000'007
-#define OO 2'000'000'000
 #define EPS 1e-9
+constexpr int INF = 1 << 30, Mod = 1e9 + 7;
+constexpr ll LINF = 1LL << 62;
 #define PI acos(-1)
+template < typename T = int > using Pair = pair < T, T >;
+vector < string > RET = {"NO", "YES"};
 
 template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
-    for (auto &x: v) in >> x;
+    for (auto &x : v) in >> x;
     return in;
 }
 
 template < typename T = int > ostream& operator << (ostream &out, const vector < T > &v) { 
-    for (const T &x: v) out << x << ' '; 
+    for (const T &x : v) out << x << ' '; 
     return out;
 }
 
-void AhMeD_HoSSaM(){
-    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout);
-    #endif
-}
+struct KMP {
 
-vector < int > Compute_Prefix(string pat){
-    vector < int > longest_prefix(sz(pat));
-    for(int i = 0, k; i < sz(pat); i++){
-        while(k > 0 && pat[k] != pat[i])
-            k = longest_prefix[k - 1];
-        if(pat[k] == pat[i]) k++;
-        else k = longest_prefix[k - 1];
-    }
-    return longest_prefix;
-}
+    string pattern;
+    vector < int > lp;
 
-void KMP(string str, string pat){
-    vector < int > longest_prefix = Compute_Prefix(pat);
-    for(int i = 0, k; i < sz(str); i++){
-        while(k > 0 && pat[k] != str[i])
-            k = longest_prefix[k - 1];
-        if(pat[k] == str[i]) k++;
-        if(k == sz(pat))
-            k = longest_prefix[k - 1];
+    KMP(const string& str = "") : pattern(str) {
+        build();
     }
-}
+
+    void build(){
+        int n = sz(pattern);
+        lp.resize(n);
+        for(int i = 1; i < n; i++)
+            lp[i] = failure(lp[i - 1], pattern[i]);
+    }
+
+    int failure(int idx, char nxt){
+        while(idx > 0 && pattern[idx] != nxt)
+            idx = lp[idx - 1];
+        return idx + (pattern[idx] == nxt);
+    }
+
+    vector < int > match(const string& str){
+        int n = sz(str), m = sz(pattern);
+        vector < int > ret;
+        for(int i = 0, k = 0; i < n; i++){
+            k = failure(k, str[i]);
+            if(k == m)
+                ret.push_back(i - m + 1); // 0-based indexing
+        }
+
+        for(auto& idx : ret)
+            idx++; // 1-based indexing
+        
+        return ret;
+    }
+
+    vector < int > count(const string& str){ // count number of occurrences of each prefix
+        int n = sz(str), m = sz(pattern);
+        vector < int > cnt(m + 1);
+        for(int i = 1, k = 0; i < n; i++){
+            k = failure(k, str[i]);
+            cnt[k]++;
+        }
+
+        for(int i = m - 1; i > 0; i--)
+            cnt[lp[i - 1]] += cnt[i]; // number of occurrences of prefix of length lp[i - 1] is at least cnt[i]
+ 
+        for(int i = 0; i <= m; i++) 
+            cnt[i]++; // add the whole substring itself
+ 
+        return cnt;
+    }
+
+    vector < int > get_prefixes(){
+        vector < int > indexes = {sz(pattern)};
+        for(int i = lp[sz(pattern) - 1]; i > 0; i = lp[i - 1])
+            indexes.push_back(i);
+        sort(all(indexes)); // indexes of prefixes increasing
+        return indexes;
+    }
+};
 
 void Solve(){
     
 }
 
 int main(){
-    AhMeD_HoSSaM();
-    int t = 1;
-    //cin >> t;
-    while(t--)
+    ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int test_cases = 1;
+    // cin >> test_cases;
+    for(int tc = 1; tc <= test_cases; tc++){
+        // cout << "Case #" << tc << ": ";
         Solve();
+    }
     return 0;
-} 
+}
